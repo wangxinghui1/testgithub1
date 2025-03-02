501530e2aba66a1b4142716f513fa6d85bf02584
wangxinghui1 <79625116+wangxinghui1@users.noreply.github.com>
2025-03-02 11:23:44 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,11 @@
-from importlib.metadata import entry_points
-from setuptools import setup,find_packages
+from setuptools import setup
+
 setup(
-    name="hello_python",
-    version="0.1.0",
-    author="yuepo",
-    description="A simple hello python package",
-    packages=find_packages(where="src"),
-    package_dir={"":"src"},
-    entry_points={
-        "console_scripts":[
-            "hellopython=testpython:say_hello"
-        ]
-    },
+    name="printforloop",
+    version="0.1",
+    author="Your Name",  # 替换为你的名字
+    author_email="your.email@example.com",  # 替换为你的邮箱
+    description="A package demonstrating a for-loop print function",
+    py_modules=['printforloop'],  # 直接打包根目录的 .py 文件
+    install_requires=[],  # 添加你的依赖库（如果有）
 )
